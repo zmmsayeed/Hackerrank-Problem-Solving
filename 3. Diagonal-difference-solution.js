1cc3b2918d5c5ea9d3fbcb512deae613ee1fbbee
Zimam Sayeed ~Ubuntu <zmmsayeed@gmail.com>
2021-01-10 13:10:15 +0530

    Added explanation comments in the solution

--- 3. Diagonal-difference-solution.js
+++ 3. Diagonal-difference-solution.js
@@ -1,11 +1,12 @@
 function diagonalDifference(arr) {
     // Write your code here
-    
     let newArr = [0, 0];
     
     // find the size of the matrix
     let length = arr.length - 1;
     
+    // line 10 - adds the elements for the first diagonal
+    // line 11 - adds the elements for the second diagonal
     arr.map((elem, index) => {
         newArr[0] += elem[index];
         newArr[1] += elem[length - index]
